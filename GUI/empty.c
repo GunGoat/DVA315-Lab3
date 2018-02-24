4f72ec252bf230df2316ff6a15d18ef75e61d41f
Christian Mortensen <Chrille@jinxed.se>
2018-02-24 15:33:40 +0100

    move some code from old client into gui client

--- GUI/empty.c
+++ GUI/empty.c
@@ -1,7 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include "resource.h"
+#include "../Shared files/wrapper.h"
 
 #define MAINWINDOW 0
 #define ADDWINDOW 1
@@ -9,6 +12,12 @@
 HWND *dialog;
 MSG msg;
 BOOL ret;
+
+HANDLE writeslot;
+
+void responseThread(char* pid);
+void sendPlanetToServer(planet_data* p);
+int setupMailboxesAndThreads();
 
 INT_PTR CALLBACK DialogProc(HWND hDlg, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -90,6 +99,9 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdL
 
 	//MessageBox(NULL, "It works man?\n", "A cool mbop", 0);
 
+	if (setupMailboxesAndThreads() == -1)
+		return 0;
+
 	dialog = malloc(sizeof(HWND) * 2);
 	dialog[MAINWINDOW] = CreateDialogParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DialogProc, 0);
 	dialog[ADDWINDOW] = CreateDialogParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG2), 0, DialogProc, 0);
@@ -109,6 +121,69 @@ int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdL
 
 	}
 
-	return 0;
+	mailslotClose(writeslot);
 
+	return 0;
+}
+
+int setupMailboxesAndThreads() {
+
+	// Connect to the mailbox setup by the server
+	writeslot = mailslotConnect("mailbox");
+	if (writeslot == INVALID_HANDLE_VALUE) {
+		printf("Failed to get a handle to the mailslot!!\nHave you started the server?\n");
+		return -1;
+	}
+
+	char pid[30];
+	sprintf(pid, "%ul", GetCurrentProcessId());
+
+	// Create server response listener thread
+	if (threadCreate(responseThread, pid) == 0)
+		return -1;
+
+	return 0;
+}
+
+void sendPlanetToServer(planet_data* p) {
+	// TODO: This is pretty much a copy paste of old client code, needs to be updated to work maybe!
+	printf("Adding planet \"%s\" with position (%lf, %lf), velocity (%lf, %lf), mass %lf and life %d\n",
+		p->name, p->sx, p->sy, p->vx, p->vy, p->mass, p->life);
+	DWORD bytesWritten = mailslotWrite(writeslot, p, sizeof(planet_type));
+
+	if (bytesWritten != -1)
+		printf("Data sent to server (bytes = %d)\n\n", bytesWritten);
+	else
+		printf("Failed sending data to server\n\n");
+
+	/*if (strlen(unread) > 0) {
+		unread[strlen(unread) - 2] = '\0';
+		printf("The planet(s): %s died while you were away.\n\n", unread);
+		unread[0] = '\0';
+	}*/
+}
+
+void responseThread(char* pid) {
+	DWORD bytesRead;
+	HANDLE readslot = mailslotCreate(pid);
+
+	//char buffer[1024];
+	planet_type* buffer = malloc(sizeof(planet_type));
+	while (1) {
+
+		bytesRead = mailslotRead(readslot, buffer, sizeof(planet_type));
+		if (bytesRead != 0) {
+			planet_type *pt = (planet_type *)malloc(sizeof(planet_type));
+			memcpy(pt, buffer, sizeof(planet_type));
+			/*if (lock == 1) {
+				strcat(unread, strcat(pt->name, ", "));
+			}
+			else {
+				printf("\n\"%s\" is dead! Long live \"%s\"\n", pt->name, pt->name);
+				printf("\nPress 1 to create planet\nPress 2 to randomize planet\n");
+			}*/
+			free(pt);
+		}
+		Sleep(1000);
+	}
 }
